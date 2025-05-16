0df762f59f4d2fc690c9ff5fe4e3bde7b99da798
Ruben Villalobos <rubenvillalobos@Mac-mini.local>
2025-05-15 22:40:41 -0700

    Fix: debug and fallback for PORT

--- launch.sh
+++ launch.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-echo "PORT is: $PORT"
+
+if [ -z "$PORT" ]; then
+  echo "🚨 \$PORT is not set! Defaulting to 10000"
+  PORT=10000
+fi
+
+echo "✅ Starting app on PORT $PORT"
 uvicorn border_app:app --host 0.0.0.0 --port $PORT
